5995db7b9a24dec80db3c1574e006fe298bf067d
Nguyễn Việt Đức <ducnv@tasc.vn>
2023-10-26 21:49:40 +0700

    update api get list doctor

--- src/main/java/com/example/spring_jwt/service/UserService.java
+++ src/main/java/com/example/spring_jwt/service/UserService.java
@@ -3,6 +3,8 @@ package com.example.spring_jwt.service;
 import com.example.spring_jwt.entities.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import java.util.List;
+
 public interface UserService extends GeneralService<User>, UserDetailsService {
 
     User findByEmail(String email);
@@ -10,6 +12,8 @@ public interface UserService extends GeneralService<User>, UserDetailsService {
     User getUserById(Integer id);
 
     void deleteUser(Integer id);
+
+    List<User> findAllByRole(String role);
 
 
 }
