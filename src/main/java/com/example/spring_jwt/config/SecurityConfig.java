62edc5cf2f33d1cb4b4dac74a13500fd4159cb53
baclx <lxbac1105@gmail.com>
2023-11-01 12:47:22 +0700

    update security

--- src/main/java/com/example/spring_jwt/config/SecurityConfig.java
+++ src/main/java/com/example/spring_jwt/config/SecurityConfig.java
@@ -66,11 +66,11 @@ public class SecurityConfig {
         http.csrf().ignoringAntMatchers("/**");
         http.httpBasic().authenticationEntryPoint(restServicesEntryPoint());
         http.authorizeRequests()
-                .antMatchers("/user/updateUser/**").hasAnyRole("ROLE_ADMIN")
-                .antMatchers("/admin/updateUserToDoctor").hasAnyRole("ROLE_ADMIN")
-                .antMatchers("/api/specialization/create").hasAnyRole("ROLE_ADMIN")
-                .antMatchers("/api/medicalRecord/create").hasAnyRole("ROLE_DOCTOR")
-                .antMatchers("/**", "/api/**","/admin/**").permitAll()
+//                .antMatchers("/user/updateUser/**").hasAnyRole("ROLE_ADMIN")
+//                .antMatchers("/admin/updateUserToDoctor").hasAnyRole("ROLE_ADMIN")
+//                .antMatchers("/api/specialization/create").hasAnyRole("ROLE_ADMIN")
+//                .antMatchers("/api/medicalRecord/create").hasAnyRole("ROLE_DOCTOR")
+//                .antMatchers("/**", "/api/**","/admin/**").permitAll()
                 .anyRequest().permitAll()
                 //.anyRequest().authenticated()
                 .and().csrf().disable();
